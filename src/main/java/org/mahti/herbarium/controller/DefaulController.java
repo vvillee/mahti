112606de5427f04d682911412b58f42f8aa5ea19
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2014-11-30 21:01:58 +0200

    now default controller do not set extra slashes to routes

--- src/main/java/org/mahti/herbarium/controller/DefaulController.java
+++ src/main/java/org/mahti/herbarium/controller/DefaulController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/*")
 public class DefaulController {
     
     @Autowired
